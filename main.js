0c67255aaed520d0f910b597488336f6d218f896
cryanblack31 <cryanblack31@gmail.com>
2026-08-30 10:29:37 -0500

    Implement logging for main title and list items
    Add JavaScript code to log main title and list items

---
+++ main.js
@@ -0,0 +1,15 @@
+const mainTitle = document.getElementById("main-title");
+console.log(mainTitle);
+
+const titleByCss = document.querySelector("#main-title");
+console.log(titleByCss);
+
+const items = document.querySelectorAll(".list-box ul");
+console.log(items);
+console.log(items.length);
+console.log(items[0], items[1]);
+
+const listItems = document.querySelectorAll("li");
+console.log(listItems);
+console.log(listItems.length);
+console.log(listItems[0]);
